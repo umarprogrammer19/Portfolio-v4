62887a4d01778b4bf8911d00b5a3a1b80ded29c1
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-24 10:55:27 +0500

    }

---
+++ src/components/couter.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useEffect, useRef, useState } from "react"
+import { useInView } from "framer-motion"
+
+interface CounterProps {
+    from: number
+    to: number
+    className?: string
+}
+
+export function Counter({ from, to, className }: CounterProps) {
+    const [count, setCount] = useState(from)
+    const ref = useRef(null)
+    const isInView = useInView(ref)
+
+    useEffect(() => {
+        if (isInView) {
+            const duration = 2000 // 2 seconds
+            const steps = 60
+            const stepValue = (to - from) / steps
+            const stepDuration = duration / steps
+
+            let currentStep = 0
+
+            const timer = setInterval(() => {
+                currentStep++
+                setCount(Math.min(from + Math.round(stepValue * currentStep), to))
+
+                if (currentStep >= steps) {
+                    clearInterval(timer)
+                }
+            }, stepDuration)
+
+            return () => clearInterval(timer)
+        }
+    }, [from, to, isInView])
+
+    return (
+        <span ref={ref} className={className}>
+            {count}
+        </span>
+    )
+}
+
